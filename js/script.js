f270d4b2a4e24a8375037b3c954d040c7c7f7255
Lorenzo-Arnau <skyline03@hotmail.it>
2021-01-28 13:02:59 +0100

    done and works

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var colore = 'red';
+var app = new Vue({
+ el: '#root',
+ data:{
+   arrayInput:[],
+   inputMessage:'',
+ },
+ methods:{
+   pushing : function() {
+     if (this.inputMessage !== '') {
+       this.arrayInput.push(this.inputMessage);
+       this.inputMessage = ''
+     }
+     return this.arrayInput
+   },
+   eliminate : function(i) {
+    this.arrayInput = this.arrayInput.filter((element,index)=>{
+       return index !== i
+     })
+   }
+},
+});
+
+Vue.config.devtools = true;
